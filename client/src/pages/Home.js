2835a61102da804845a20575508cc8cf2e2be6dc
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-11 18:00:49 -0500

    Implement Home component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import logo from '../assets/images/Huntr.mp4'
+import main from '../assets/images/main.jpeg'
+
+const Home = () => {
+  return (
+    <div>Landing Page</div>
+  )
+}
+
+export default Home;
