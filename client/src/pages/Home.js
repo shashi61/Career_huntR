f4683140e89cd58be99108161c6b0ca75c8b99c7
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-11 18:39:21 -0500

    adding logo and main images with some dummy data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,31 @@
-import logo from '../assets/images/Huntr.mp4'
+import logo from '../assets/images/logo.png'
 import main from '../assets/images/main.jpeg'
 
 const Home = () => {
   return (
-    <div>Landing Page</div>
+    <main>
+      <nav>
+        <img src={logo} alt="Career HuntR" className="logo" width="70" height="50" />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            carrer <span>track</span> app
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <button className='btn btn-hero'>
+            Login/Register
+          </button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </main>
   )
 }
 
