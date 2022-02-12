995a4f5b22a2d5e78a8f6d96cdf44ac275967cd2
Shashi Bawa <shashibawa104@gmail.com>
2022-02-12 12:17:51 -0500

    Add navigation links using Link component

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <BrowserRouter>
+      <BrowserRouter>
+       <nav>
+        <Link to='/'>Dashboard</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/home'>Home</Link>
+      </nav>
         <Routes>
           <Route path="/" element={<div>Dashboard</div>} />
           <Route path="/register" element={<div>Register</div>} />
           <Route path="/home" element={<Home />} />
           <Route path="*" element={<div>Error</div>} />
         </Routes>
-    </BrowserRouter>
-    </div>
+      </BrowserRouter>
     
+
   );
 }
 
