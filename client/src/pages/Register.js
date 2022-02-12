16628e7ca1951354288aa5e41eec3a757b70db7e
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-12 15:11:41 -0500

    Create register form using state

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,55 @@
-const Register = () =>{
-    return(
-        <div><h1>Register</h1></div>
-    )
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/registerCss'
+
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
+
+
+function Register() {
+  const [values, setValues] = useState(initialState)
+
+  // global context and useNavigate later
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+
+          <input
+            type='text'
+            value={values.name}
+            name='name'
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  )
 }
 export default Register;
