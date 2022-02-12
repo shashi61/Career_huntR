0021973b072271126cc2883d02427487e8189eb9
Shashi Bawa <shashibawa104@gmail.com>
2022-02-12 13:10:58 -0500

    Import img and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Home, Error, Dashboard, Register } from "./pages";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,6 +1,15 @@
-const Error = () =>{
-    return(
-        <div><h1>Error</h1></div>
+import { Link } from 'react-router-dom';
+import img from '../assets/images/error404.svg'
+import Wrapper from '../assets/wrappers/errorCss'
+
+const Error = () => {
+    return (
+        <Wrapper className='full-page'>
+            <div>
+                <img src={img} alt='not found' />
+                <Link to='/'>back home</Link>
+            </div>
+         </Wrapper>
     )
 }
 export default Error;
