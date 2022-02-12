e066d4533e7dd3052ddf464dcb5291669bf8c108
Shashi Bawa <shashibawa104@gmail.com>
2022-02-12 12:07:07 -0500

    Import and add client side routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Career huntR</h1>
-      <Home/>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Dashboard</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<div>Error</div>} />
+        </Routes>
+    </BrowserRouter>
     </div>
     
   );
