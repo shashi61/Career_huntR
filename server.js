0849553e92884ce23a5052c7a1b168153b8201b0
Shashi Bawa <shashibawa104@gmail.com>
2022-02-12 22:30:59 -0500

    Implement custom middleware for error handling

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-console.log("Server running");
 import express from 'express'
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from './middleware/error-handler.js'
 const app = express()
 
 app.get('/', (req, res) => {
+    throw new Error('error')
   res.send('Welcome!')
 })
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
