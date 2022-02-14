fcdd64a66ab27f44174985abae330539230dc704
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-14 18:03:27 -0500

    Create dummy routes for jobs

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ dotenv.config()
 import connectDB from './db/connect.js'
 
 
-import authRouter from './routes/authRoute.js'
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRoutes.js'
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js"
@@ -23,6 +24,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter)
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
