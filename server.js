ea54e71bfa766ada75e4a933c34d96dcde5b72aa
Shashi Bawa <shashibawa104@gmail.com>
2022-02-14 13:16:16 -0500

    Connect mongo db to server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 //env
 import dotenv from 'dotenv'
 dotenv.config()
+//connect database
+import connectDB from './db/connect.js'
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js"
@@ -19,4 +21,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
