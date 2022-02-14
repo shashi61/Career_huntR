37890fe1941f0e3ab83f6828bc76edf61943b351
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-14 17:34:13 -0500

    Setup structure for authControllers and authRouters

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const app = express()
 //env
 import dotenv from 'dotenv'
 dotenv.config()
-//connect database
+
+// db and authentication
 import connectDB from './db/connect.js'
+
+
+import authRouter from './routes/authRoute.js'
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js"
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
     throw new Error('error')
   res.send('Welcome!')
 })
+
+app.use('/api/v1/auth', authRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
