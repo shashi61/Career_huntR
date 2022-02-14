c6c8c8c557fdc9802bb643f5b75aac8026c25a11
Jovita <jovitaspais@gmail.com>
2022-02-14 15:05:48 -0500

    implement clear alert functionality

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,5 @@
 import React, { useReducer, useContext } from 'react'
-import { DISPLAY_ALERT } from './actions'
+import { DISPLAY_ALERT, CLEAR_ALERT } from './actions'
 
 import reducer from './reducer'
 
@@ -19,6 +19,15 @@ const AppProvider = ({ children }) => {
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT })
+    clearAlert()
+  }
+  
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({
+        type: CLEAR_ALERT,
+      })
+    }, 3000)
   }
 
   return (
