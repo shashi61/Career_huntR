c4061f4d93da7713e57a193be8a2f77f20b8cc2e
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-15 18:10:46 -0500

    Define action type in reduser.js

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -6,6 +6,8 @@ import {
 	REGISTER_USER_ERROR,
 } from "./actions";
 
+import { initialState } from './appContext';
+
 const reducer = (state, action) => {
 	if (action.type === DISPLAY_ALERT) {
 		return {
@@ -15,14 +17,42 @@ const reducer = (state, action) => {
 			alertText: "Please provide all values!",
 		};
   }
-		if (action.type === CLEAR_ALERT) {
+	if (action.type === CLEAR_ALERT) {
 			return {
-				...state,
-				showAlert: false,
-				alertType: "",
-				alertText: "",
-			};
+			...state,
+			showAlert: false,
+			alertType: "",
+			alertText: "",
+		};
+	}
+	if (action.type === REGISTER_USER_BEGIN) {
+			return {
+				...state, isLoading: true 
+			}
+	}
+	if (action.type === REGISTER_USER_SUCCESS) {
+		return {
+			...state,
+			user: action.payload.user,
+			token: action.payload.token,
+			userLocation: action.payload.location,
+			jobLocation: action.payload.location,
+			isLoading: false,
+			showAlert: true,
+			alertType: 'success',
+			alertText: 'User Created! Redirecting...',
 		}
+	}
+	if (action.type === REGISTER_USER_ERROR) {
+		return {
+			...state,
+			isLoading: false,
+			showAlert: true,
+			alertType: 'danger',
+			alertText: action.payload.msg,
+		}
+	}
+
 	
 	throw new Error(`no such action :${action.type}`);
 };
