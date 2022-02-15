866bb31580f228586ef037ad587f481ac791e458
Shashi Bawa <shashibawa104@gmail.com>
2022-02-15 13:29:14 -0500

    import bcrypt and function to hash password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import  mongoose  from "mongoose";
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Mongoose.Schema({
   name: {
@@ -31,8 +32,9 @@ const UserSchema = new Mongoose.Schema({
   },
 })
 //will trigger in the authcontroller where a user is being created
-UserSchema.pre('save', function(){
-  console.log(this.password);
+UserSchema.pre('save', async function(){
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt)
 })
 
 export default mongoose.model('User', UserSchema);
