90743fb7b7d66e92ca2b4f157fb86a3e1d1fee40
Jovita <jovitaspais@gmail.com>
2022-02-15 12:20:39 -0500

    add error handler for duplicate emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,10 @@ const register = async (req, res) =>{
         //invoke the instance CustomeAPIError
         throw new BadRequestError('please provide all values')
       }
-      
+      const userAlreadyExists = await User.findOne({email});
+      if(userAlreadyExists){
+        throw new BadRequestError ('Email already is use')
+      }
       
       const user = await User.create(name, email, password)
       res.status(StatusCodes.CREATED).json({user})
