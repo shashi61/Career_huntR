8f0985fee14ff83ca2be6c35f533c3479257e238
Jovita <jovitaspais@gmail.com>
2022-02-15 13:18:03 -0500

    install morgan package middleware to handle http logs

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,7 +1,7 @@
-import {StatusCodes} from 'http-status-codes' 
+import { StatusCodes } from 'http-status-codes' 
 import CustomeAPIError from './custom-api.js'
 
-class BadRequestError extends CustomeAPIError{
+class BadRequestError extends CustomeAPIError {
   constructor(message){
     super(message) 
     this.statusCode = StatusCodes.BAD_REQUEST 
