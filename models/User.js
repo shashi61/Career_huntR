339ee200bc957252880d5017c7609c240a58e3a5
Shashi Bawa <shashibawa104@gmail.com>
2022-02-15 15:15:30 -0500

    Update the secret key and lifetime with encrypted key

--- models/User.js
+++ models/User.js
@@ -44,7 +44,7 @@ UserSchema.pre('save', async function(){
  this.password = await bcrypt.hash(this.password, salt)
 })
 UserSchema.methods.createJWT = function(){
-  return jwt.sign({userId: this._id}, 'jwtSecret', {expiresIn: "1d"})
+  return jwt.sign({userId: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
 }
 
 export default mongoose.model('User', UserSchema);
