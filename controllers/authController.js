d7f72d67775054ee6d4b5a629de1a64b724fd871
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-16 18:18:44 -0500

    Implement Login controller functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError, NotFoundError } from '../errors/index.js'
+import { BadRequestError,UnAuthenticatedError } from '../errors/index.js'
 
 
 const register = async (req, res) =>{
@@ -21,12 +21,34 @@ const register = async (req, res) =>{
 
       //invoking the token
      const token = user.createJWT()
-      res.status(StatusCodes.CREATED).json({ user:{email:user.email, lastname:user.lastname, location:user.location, name:user.name }, token, location: user.location })
+      res.status(StatusCodes.CREATED).json({ user:{ email:user.email, lastname:user.lastname, location:user.location, name:user.name }, token, location: user.location })
     
 }
-const login = async (req, res) =>{
-  res.send("login user")
+
+const login = async (req, res) => {
+
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ email }).select('+password')
+
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  console.log(user);
+
+  const isPasswordCorrect = await user.comparePassword(password)
+
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
+  res.send('login user');
 }
+
 const updateUser = async (req, res) =>{
   res.send("update user")
   user.save()
