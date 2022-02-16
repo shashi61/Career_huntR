ea6613310835d5338f99043dd86ad5728caf8bd0
Shashi Bawa <shashibawa104@gmail.com>
2022-02-16 14:59:41 -0500

    Add and import css for the shared components

---
+++ client/src/components/BigSidebar.js
@@ -0,0 +1,12 @@
+import Wrapper from '../assets/wrappers/bigBarCss'
+import {FaHome} from 'react-icons/fa'
+const BigSidebar = () => {
+  return (
+    <Wrapper>
+      <h4>BigSidebar</h4>
+      <FaHome />
+    </Wrapper>
+  )
+}
+
+export default BigSidebar
---
+++ client/src/components/SmallSidebar.js
@@ -0,0 +1,12 @@
+import Wrapper from '../assets/wrappers/smallBarCss'
+import {FaHome} from 'react-icons/fa'
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      <h4>SmallSidebar</h4>
+      <FaHome />
+    </Wrapper>
+  )
+}
+
+export default SmallSidebar
