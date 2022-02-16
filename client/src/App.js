30e5b79ce8a8a33e938381812369262b9d217a1d
Shashi Bawa <shashibawa104@gmail.com>
2022-02-16 12:44:10 -0500

    Add all the nested routes on the app.js to the dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import { Home, Error, Register } from "./pages";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {AllJobs, Profile, SharedLayout, Stats, AddJob} from './pages/dashboard'
+import { AllJobs, Profile, SharedLayout, Stats, AddJob } from './pages/dashboard'
 
 function App() {
   return (
-      <BrowserRouter>
-       {/* <nav>
-        <Link to='/'>Dashboard</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/home'>Home</Link>
-      </nav> */}
-        <Routes>
-          <Route path="/" element={<div>dashboard</div>} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard">
+          <Route path="stats" element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+
 
   );
 }
