1af7be8273add91bcaf28eaa7df83784c0ce9563
Jovita <jovitaspais@gmail.com>
2022-02-17 10:41:32 -0500

    set up create job function

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,20 @@
-const createJob =  async (req, res) => { 
-  res.send ('create job')
+import { StatusCodes } from 'http-status-codes'
+import Job from '../models/Job.js'
+import {
+  BadRequestError,
+  UnAuthenticatedError,
+} from '../errors/index.js'
+
+
+  const createJob = async (req, res) => {
+  const { position, company } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values')
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const deleteJob =  async (req, res) => { 
