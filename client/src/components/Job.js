59cff56427957e3bbd621a204c3fe5af969d246b
Jovita <jovitaspais@gmail.com>
2022-02-17 17:33:21 -0500

    import moment to add date stamp

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,6 +1,13 @@
-const Job = ({company}) => {
+import moment from 'moment'
+
+const Job = ({ company, createdAt }) => {
+  let date = moment(createdAt)
+  date = date.format('MMM Do, YYYY') //Do is date
   return (
-    <h5>{company}</h5>
+    <div>
+      <h5>{company}</h5>
+      <h5>{date}</h5>
+    </div>
   )
 }
 
