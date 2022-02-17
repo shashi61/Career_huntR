59391d10478b638d3cb9db141eacdcd59f79bf5e
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-17 11:17:33 -0500

    Modify auth to check condition whether header start with Bearer Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,24 @@
+import jwt from 'jsonwebtoken'
 import { UnAuthenticatedError } from '../errors/index.js'
 
 const auth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    // why, well is it 400 or 404?
-    // actually 401
-    throw new UnAuthenticatedError('Authentication Invalid')
+  // check header
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid')
   }
-  next()
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    // console.log(payload)
+    // attach the user request object
+    // req.user = payload
+    req.user = { userId: payload.userId }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid')
+  }
 }
 
 export default auth
