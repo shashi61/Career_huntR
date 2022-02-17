f404d23a435aca297284edf5e79a4b2b49028738
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-17 12:35:32 -0500

    Modified path

--- models/User.js
+++ models/User.js
@@ -41,8 +41,12 @@ const UserSchema = new mongoose.Schema({
 })
 //will trigger in the authcontroller where a user is being created
 UserSchema.pre('save', async function(){
-  // const salt = await bcrypt.genSalt(10);
-  // this.password = await bcrypt.hash(this.password, salt)
+  // console.log(this.modifiedPaths())
+  // console.log(this.isModified('name'))
+
+  if (!this.isModified('password')) return
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
  })
  UserSchema.methods.createJWT = function(){
   return jwt.sign({userId: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_LIFETIME})
