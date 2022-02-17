00dcd900188429be47e6ea030c3e0ecafee7a48a
jubhavsar <juhi_bhavsar@yahoo.com>
2022-02-17 10:58:04 -0500

    Modify auth.js and check updateUser routes in Postman while generating Bearer token for Authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,5 @@
 const auth = async (req, res, next) => {
-  const headers = req.headers;
   const authHeader = req.headers.authorization;
-  console.log(headers)
   console.log(authHeader)
   next()
 }
