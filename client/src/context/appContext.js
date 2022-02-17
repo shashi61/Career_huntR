ed392acdb72dc93dab28b9af0c377026ec41d5e2
Shashi Bawa <shashibawa104@gmail.com>
2022-02-17 13:03:23 -0500

    add axios interceptors

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -39,7 +39,32 @@ const AppProvider = ({ children }) => {
   // reducer function will handle dispatch
   const [state, dispatch] = useReducer(reducer, initialState);
   
-  axios.defaults.headers.common['Authorization'] = `Bearer ${state.token}`
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  })
+// request interceptor
+authFetch.interceptors.request.use(
+  (config) => {
+    //config.headers.common['Authorization'] = `Bearer ${state.token}`
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+// response interceptor
+authFetch.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    console.log(error.response)
+    if (error.response.status === 401) {
+      console.log('AUTH ERROR')
+    }
+    return Promise.reject(error)
+  }
+)
 
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
@@ -135,13 +160,6 @@ const AppProvider = ({ children }) => {
     removeUserFromLocalStorage()
   }
 
-  const authFetch = axios.create({
-    baseURL: '/api/v1',
-    headers: {
-      Authorization: `Bearer ${state.token}`,
-    },
-  })
-  
   const updaterUser = async (currentUser) => {
     try {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser)
