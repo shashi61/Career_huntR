a0367cca55998fc03fb1c87a06ac79c2fdef031f
Shashi Bawa <shashibawa104@gmail.com>
2022-02-17 23:35:33 -0500

    Error handling refactoring

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,11 +1,10 @@
-import { StatusCodes } from 'http-status-codes' 
-import CustomeAPIError from './custom-api.js'
+import { StatusCodes } from 'http-status-codes'
+import CustomAPIError from './custom-api.js'
 
-class BadRequestError extends CustomeAPIError{
-  constructor(message){
-    super(message) 
-    this.statusCode = StatusCodes.BAD_REQUEST 
-    
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message)
+    this.statusCode = StatusCodes.BAD_REQUEST
   }
 }
 
--- errors/custom-api.js
+++ errors/custom-api.js
@@ -1,12 +1,7 @@
-
-
-//use Error class
-class CustomeAPIError extends Error {
-  constructor(message){
+class CustomAPIError extends Error {
+  constructor(message) {
     super(message)
-    //add status code by adding property StatusCodes on the instance 
-    // this.statusCode = StatusCodes.BAD_REQUEST //set to 400 error
   }
 }
 
-export default CustomeAPIError
+export default CustomAPIError
