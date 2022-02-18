f085b9571d25cae65e463667d7918acd069725e8
Shashi Bawa <shashibawa104@gmail.com>
2022-02-17 23:40:21 -0500

    Refactoring

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express'
-import { updateUser } from '../controllers/authController.js'
 const router = express.Router()
 
-import { createJob, deleteJob, getAllJobs, updateJob, showStats } from '../controllers/jobsController.js'
+import {
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  showStats,
+} from '../controllers/jobsController.js'
 
-router.route('/').post(createJob).get(getAllJobs);
-router.route('/stats').get(showStats);
-router.route('/:id').delete(deleteJob).patch(updateUser);
+router.route('/').post(createJob).get(getAllJobs)
+// remember about :id
+router.route('/stats').get(showStats)
+router.route('/:id').delete(deleteJob).patch(updateJob)
 
-
-export default router;
-
+export default router
--- server.js
+++ server.js
@@ -1,47 +1,55 @@
 import express from 'express'
-import morgan from 'morgan'
-
 const app = express()
-
-//env
 import dotenv from 'dotenv'
 dotenv.config()
-
 import 'express-async-errors'
+import morgan from 'morgan'
 
-// db and authentication
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
+// hello
+// db and authenticateUser
 import connectDB from './db/connect.js'
 
-
+// routers
 import authRouter from './routes/authRoutes.js'
 import jobsRouter from './routes/jobsRoutes.js'
 
-//middleware
-import notFoundMiddleware from "./middleware/not-found.js"
+// middleware
+import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import authenticateUser from './middleware/auth.js'
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'))
 }
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome!' })
- 
-})
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.get('/api/v1', (req, res) => {
-  res.json({ msg: 'API !' })
-})
+// only when ready to deploy
+// app.use(express.static(path.resolve(__dirname, './client/build')))
 
-app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/jobs', jobsRouter);
-app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+// only when ready to deploy
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+// })
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
